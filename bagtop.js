03286eaf59ba0dd6a80aae81250d6996ab1f3001
帳號 <user@Userde-MacBook-Air.local>
2019-09-20 23:00:24 +0800

    RWD

---
+++ bagtop.js
@@ -0,0 +1,17 @@
+$(document).ready(function () {
+    $("#btntop").click(function (e) {
+        $("html,body").animate({ scrollTop: 0 }, 1000);
+    });
+    //alert();
+    $(window).scroll(function () {
+        ;
+        if ($(window).scrollTop() > 660) {
+            $("#btntop").fadeIn(500)
+        } else {
+            $("#btntop").fadeOut(500)
+
+        }
+
+    });
+});
+
