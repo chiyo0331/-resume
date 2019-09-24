0f6a40c362104f377ef870e68df9970b2c890a4a
帳號 <user@Userde-MacBook-Air.local>
2019-09-24 18:40:17 +0800

    work RWD

--- topnav.js
+++ topnav.js
@@ -1,8 +1,17 @@
+// When the user scrolls the page, execute myFunction 
+window.onscroll = function() {myFunction()};
+
+// Get the navbar
+var navbar = document.getElementById("myNavbar-s");
+
+// Get the offset position of the navbar
+var sticky = navbar.offsetTop;
+
+// Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
 function myFunction() {
-    var x = document.getElementById("myTopnav");
-    if (x.className === "topnav") {
-      x.className += " responsive";
-    } else {
-      x.className = "topnav";
-    }
+  if (window.pageYOffset >=600) {
+    navbar.classList.add("sticky")
+  } else {
+    navbar.classList.remove("sticky");
   }
+}
